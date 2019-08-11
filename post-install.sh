2ad8b9e4fda25ecf16c214e0a93dc03d386a9cb7
Ayberk Aydin <e187569@metu.edu.tr>
2019-08-11 20:42:27 +0300

    update

--- post-install.sh
+++ post-install.sh
@@ -27,69 +27,77 @@ sudo pacman -S bumblebee mesa nvidia xf86-video-intel --noconfirm
 ### Install apps      #############
 ###################################
 
-sudo pacman -S pacman-contrib      --noconfirm
-sudo pacman -S acpi                --noconfirm
-sudo pacman -S rsync               --noconfirm
-sudo pacman -S mlocate             --noconfirm
-sudo pacman -S openssh             --noconfirm
-sudo pacman -S arandr              --noconfirm
-sudo pacman -S zip                 --noconfirm
-sudo pacman -S transmission-qt     --noconfirm
-sudo pacman -S easytag             --noconfirm
-sudo pacman -S eog                 --noconfirm
-sudo pacman -S udiskie             --noconfirm
-sudo pacman -S audacity            --noconfirm
-sudo pacman -S neofetch            --noconfirm
-sudo pacman -S deadbeef            --noconfirm
-sudo pacman -S vlc                 --noconfirm
-sudo pacman -S mpg123              --noconfirm
-sudo pacman -S libmad              --noconfirm
-sudo pacman -S unzip               --noconfirm
-sudo pacman -S chromium            --noconfirm
-sudo pacman -S i3                  --noconfirm
-sudo pacman -S dmenu               --noconfirm
-sudo pacman -S xautolock           --noconfirm
-sudo pacman -S flameshot           --noconfirm
-sudo pacman -S gvim                --noconfirm
-sudo pacman -S xclip               --noconfirm
-sudo pacman -S xsel                --noconfirm
-sudo pacman -S code                --noconfirm
-sudo pacman -S sddm                --noconfirm
-sudo pacman -S lolcat              --noconfirm
-sudo pacman -S cmatrix             --noconfirm
-sudo pacman -S termite             --noconfirm
-sudo pacman -S ranger              --noconfirm
-sudo pacman -S pcmanfm             --noconfirm
-sudo pacman -S tldr                --noconfirm
-sudo pacman -S htop                --noconfirm
-sudo pacman -S nmap                --noconfirm
-sudo pacman -S wget                --noconfirm
-sudo pacman -S python-pip          --noconfirm
-sudo pacman -S evince              --noconfirm
-sudo pacman -S nitrogen            --noconfirm
-sudo pacman -S compton             --noconfirm
-sudo pacman -S tcpdump             --noconfirm
-sudo pacman -S bind-tools          --noconfirm
-sudo pacman -S wireshark-qt        --noconfirm
-sudo pacman -S dosfstools          --noconfirm
-sudo pacman -S zip                 --noconfirm
-sudo pacman -S unzip               --noconfirm
-sudo pacman -S unrar               --noconfirm
-sudo pacman -S file-roller         --noconfirm
-sudo pacman -S libreoffice-still   --noconfirm
-sudo pacman -S fbreader            --noconfirm
-sudo pacman -S dunst               --noconfirm
-sudo pacman -S netcat              --noconfirm
-sudo pacman -S py3status           --noconfirm
-sudo pacman -S lxappearance        --noconfirm
-sudo pacman -S arc-gtk-theme       --noconfirm
-sudo pacman -S arc-icon-theme      --noconfirm
-sudo pacman -S breeze-gtk          --noconfirm
-sudo pacman -S breeze-icons        --noconfirm
-sudo pacman -S xcursor-comix       --noconfirm
-sudo pacman -S xcursor-flatbed     --noconfirm
-sudo pacman -S redshift            --noconfirm
-sudo pacman -S light               --noconfirm
+sudo pacman -S arch-install-scripts --noconfirm
+sudo pacman -S pacman-contrib       --noconfirm
+sudo pacman -S acpi                 --noconfirm
+sudo pacman -S rsync                --noconfirm
+sudo pacman -S mlocate              --noconfirm
+sudo pacman -S fbreader             --noconfirm
+sudo pacman -S openssh              --noconfirm
+sudo pacman -S arandr               --noconfirm
+sudo pacman -S zip                  --noconfirm
+sudo pacman -S transmission-qt      --noconfirm
+sudo pacman -S easytag              --noconfirm
+sudo pacman -S eog                  --noconfirm
+sudo pacman -S udiskie              --noconfirm
+sudo pacman -S audacity             --noconfirm
+sudo pacman -S neofetch             --noconfirm
+sudo pacman -S deadbeef             --noconfirm
+sudo pacman -S vlc                  --noconfirm
+sudo pacman -S mpg123               --noconfirm
+sudo pacman -S libmad               --noconfirm
+sudo pacman -S unzip                --noconfirm
+sudo pacman -S chromium             --noconfirm
+sudo pacman -S kolourpaint          --noconfirm
+sudo pacman -S i3                   --noconfirm
+sudo pacman -S dmenu                --noconfirm
+sudo pacman -S xautolock            --noconfirm
+sudo pacman -S flameshot            --noconfirm
+sudo pacman -S gvim                 --noconfirm
+sudo pacman -S xclip                --noconfirm
+sudo pacman -S xsel                 --noconfirm
+sudo pacman -S code                 --noconfirm
+sudo pacman -S sddm                 --noconfirm
+sudo pacman -S lolcat               --noconfirm
+sudo pacman -S cmatrix              --noconfirm
+sudo pacman -S termite              --noconfirm #Terminal emulator
+sudo pacman -S ranger               --noconfirm #File manager (console)
+sudo pacman -S pcmanfm              --noconfirm #File manager
+sudo pacman -S tldr                 --noconfirm
+sudo pacman -S htop                 --noconfirm
+sudo pacman -S nmap                 --noconfirm
+sudo pacman -S wget                 --noconfirm
+sudo pacman -S python-pip           --noconfirm
+sudo pacman -S evince               --noconfirm
+sudo pacman -S nitrogen             --noconfirm #Background setter
+sudo pacman -S compton              --noconfirm #Compositing
+sudo pacman -S tcpdump              --noconfirm
+sudo pacman -S bind-tools           --noconfirm
+sudo pacman -S wireshark-qt         --noconfirm
+sudo pacman -S dosfstools           --noconfirm
+sudo pacman -S zip                  --noconfirm
+sudo pacman -S unzip                --noconfirm
+sudo pacman -S unrar                --noconfirm
+sudo pacman -S engrampa             --noconfirm #Archive Manager
+sudo pacman -S libreoffice-still    --noconfirm
+sudo pacman -S fbreader             --noconfirm
+sudo pacman -S dunst                --noconfirm
+sudo pacman -S netcat               --noconfirm
+sudo pacman -S py3status            --noconfirm
+sudo pacman -S lxappearance         --noconfirm
+sudo pacman -S arc-gtk-theme        --noconfirm
+sudo pacman -S arc-icon-theme       --noconfirm
+sudo pacman -S cuda                 --noconfirm
+sudo pacman -S cudnn                --noconfirm
+sudo pacman -S breeze-gtk           --noconfirm
+sudo pacman -S breeze-icons         --noconfirm
+sudo pacman -S xcursor-comix        --noconfirm
+sudo pacman -S xcursor-flatbed      --noconfirm
+sudo pacman -S redshift             --noconfirm
+sudo pacman -S light                --noconfirm
+sudo pacman -S ctags                --noconfirm
+sudo pacman -S npm                  --noconfirm
+
 
 
 
@@ -112,6 +120,7 @@ yay -S dropbox               --noconfirm
 yay -S simplenote            --noconfirm
 yay -S pamac-aur             --noconfirm
 yay -S masterpdfeditor       --noconfirm
+yay -S downgrade             --noconfirm
 
 ## SDDM themes
 yay -S sddm-breath-theme     --noconfirm
@@ -126,7 +135,6 @@ Current=chili" | sudo tee /etc/sddm.conf
 ### Install pytorch and other scientific computing libs  ############
 #####################################################################
 
-sudo pacman -S python-pytorch-cuda --noconfirm
 sudo pip3 install numpy scipy matplotlib ipython jupyter pandas sympy nose
 
 
